8d12f6d6303998c8a2b3f3bacdc23b5cc96dd115
Theo PAYEN <tt.payen@gmail.com>
2022-11-13 16:48:17 +0100

    Update cluster.ps1

--- cluster.ps1
+++ cluster.ps1
@@ -3,28 +3,51 @@
 # 2 cree le cluster
 # 3 add host esxi on cluster
 
-$location = Get-Folder -NoRecursion
-New-DataCenter -Location $location -Name $datacenter
+function connect {
+    Set-PowerCLIConfiguration -Scope User -InvalidCertificateAction warn
+    connect-VIServer 172.20.20.5
+}
+
+function main() {
+
+    $location = Get-Folder -NoRecursion
+
+    $datacenter = "PV_Datacenter"
+    New-DataCenter -Location $location -Name $datacenter
+
+    $cluster = "PV_Cluster"
+
+    New-Cluster -Name $cluster -Location $datacenters
+
+}
 
 
-New-Cluster -Name "MyCluster" -Location "MyDatacenter"
 
 
-$server_hosts = @(
-    [pscustomobject]@{ip='172.20.20.6';login='root';password='Azerty@77'}
-    [pscustomobject]@{ip='172.20.20.7';login='root';password='Azerty@77'}
-    [pscustomobject]@{ip='172.20.20.8';login='root';password='Azerty@77'}
-)
-$server_hosts
 
 
-$server_hosts | ForEach-Object {
-        $_.ip
-        $conect_host = Connect-VIServer -Server $_.ip
-
-        Add-VMHost -Server $myServer -Name MyVMHost1 -Location MyDatacenter1 -User MyUsername1 -Password MyPassword1
-
-    }
 
 
-#Add-VMHost -Server $myServer -Name MyVMHost1 -Location MyDatacenter1 -User MyUsername1 -Password MyPassword1
+function esxi() {
+    $server_hosts = @(
+        [pscustomobject]@{ip='172.20.20.6';login='root';password='Azerty@77'}
+        [pscustomobject]@{ip='172.20.20.7';login='root';password='Azerty@77'}
+        [pscustomobject]@{ip='172.20.20.8';login='root';password='Azerty@77'}
+    )
+    $server_hosts
+    
+    
+    $server_hosts | ForEach-Object {
+            $_.ip
+            $_.login
+            $_.password
+
+            #Add-VMHost -Server $myServer -Name MyVMHost1 -Location MyDatacenter1 -User MyUsername1 -Password MyPassword1
+    
+        }
+    
+}
+main
+esxi
+
+
