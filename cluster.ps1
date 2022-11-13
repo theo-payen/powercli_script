a9a0a3c7e4a033329d795b01b125fa9529211e95
Theo PAYEN <tt.payen@gmail.com>
2022-11-13 16:56:07 +0100

    Update cluster.ps1

--- cluster.ps1
+++ cluster.ps1
@@ -8,46 +8,52 @@ function connect {
     connect-VIServer 172.20.20.5
 }
 
-function main() {
-
+function Set-cluster{
+    param (
+        $datacenter,
+        $cluster
+    )
     $location = Get-Folder -NoRecursion
 
-    $datacenter = "PV_Datacenter"
+    
     New-DataCenter -Location $location -Name $datacenter
 
-    $cluster = "PV_Cluster"
 
-    New-Cluster -Name $cluster -Location $datacenters
+    New-Cluster -Name $cluster -Location $datacenter
 
 }
 
 
-
-
-
-
-
-
-function esxi() {
-    $server_hosts = @(
-        [pscustomobject]@{ip='172.20.20.6';login='root';password='Azerty@77'}
-        [pscustomobject]@{ip='172.20.20.7';login='root';password='Azerty@77'}
-        [pscustomobject]@{ip='172.20.20.8';login='root';password='Azerty@77'}
+function New-hostesxi {
+    param (
+        $server_hosts,
+        $datacenter
     )
-    $server_hosts
-    
-    
+
     $server_hosts | ForEach-Object {
             $_.ip
             $_.login
             $_.password
-
+            Add-VMHost -Server $_.ip -Name MyVMHost1 -Location MyDatacenter1 -User MyUsername1 -Password MyPassword1
             #Add-VMHost -Server $myServer -Name MyVMHost1 -Location MyDatacenter1 -User MyUsername1 -Password MyPassword1
     
         }
     
 }
+
+function main {
+    $datacenter = "PV_Datacenter"
+    $cluster = "PV_Cluster"
+    Set-cluster -datacenter $datacenter -cluster $cluster
+
+    $server_hosts = @(
+        [pscustomobject]@{name='esx1';ip='172.20.20.6';login='root';password='Azerty@77'}
+        [pscustomobject]@{name='esx2';ip='172.20.20.7';login='root';password='Azerty@77'}
+        [pscustomobject]@{name='esx3';ip='172.20.20.8';login='root';password='Azerty@77'}
+    )
+
+    New-hostesxi -server_hosts $server_hosts -datacenter $datacenter
+
+}
+
 main
-esxi
-
-
