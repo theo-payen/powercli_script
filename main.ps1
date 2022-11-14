ed2adc74e5fcef6964f8b18fe0c8bb4d857e1411
Theo PAYEN <tt.payen@gmail.com>
2022-11-14 10:23:43 +0100

    update

--- main.ps1
+++ main.ps1
@@ -1,12 +1,26 @@
 Add-Type -AssemblyName PresentationFramework
 
 [xml]$XML = @"
-    <Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
-    Title="WPF Window" Height="480" Width="640">
-    <Button Name="MyButton" Width="80" Height="40" Content="OK" />
-    </Window>
+        <Window 
+        xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml" 
+        x:Name="Window" Title="Test Connection Window" Height="121.071" Width="460" ResizeMode="NoResize">
+
+    <Grid x:Name="LayoutRoot" Background="#FFC1C3CB">
+        <Button x:Name="Close" Content="Close" HorizontalAlignment="Left" Margin="327,10,0,0" VerticalAlignment="Top" Width="100" Height="54" />
+        <Label Name="Label_Email" Content="Your email:" HorizontalAlignment="Left" Margin="10,10,0,0" VerticalAlignment="Top" Width="130"/>
+        <TextBox Name="TextBox_Email" HorizontalAlignment="Left" Height="23" Margin="145,10,0,0" TextWrapping="Wrap" Text="youremail@domain.fr" VerticalAlignment="Top" Width="160"/>
+        <Label Name="Label_Password" Content="Your Password" HorizontalAlignment="Left" Margin="10,41,0,0" VerticalAlignment="Top" Width="130"/>
+        <PasswordBox Name="TextBox_Password" HorizontalAlignment="Left" Margin="145,41,0,0" VerticalAlignment="Top" Width="160" Height="23"/>
+    </Grid>
+</Window>
 "@
 
 $FormXML = (New-Object System.Xml.XmlNodeReader $XML)
 $Window = [Windows.Markup.XamlReader]::Load($FormXML)
-$Window.ShowDialog()
+
+$Window.FindName("Close").add_click({ 
+    $Window.Close() 
+}) 
+
+$Window.ShowDialog() | Out-Null
