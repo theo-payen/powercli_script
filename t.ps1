e33a551e56857c58b81a03b50a589ee65d5276b9
Mukil <mukil.george@icloud.com>
2022-11-30 22:38:22 +0100

    template and create

--- t.ps1
+++ t.ps1
@@ -1,4 +1,16 @@
-$myVM = Get-VM -Name "Vm_debian"
-$drsCluster=Get-DatastoreCluster "FreeNas_LabVMware"
-New-Template -VM $myVM -Name "MyTemplate" -Datastore $drsCluster -Location Datacenter2
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
+connect-VIServer 172.20.20.5 -User administrator@vsphere.local -Password Azerty@77
 
+
+
+#create template
+$myVM = Get-VM -Name "pv_linux01"
+$datastore=Get-Datastore "Storage1"
+New-Template -VM $myVM -Name "MyTemplateLinux01" -Datastore $datastore -Location Pv_Datacenter
+
+
+#create vm 
+
+$myResourcePool = Get-ResourcePool -Name Resources
+$myTemplate = Get-Template -Name MyTemplateLinux01
+New-VM -Name pv_linux02 -Template $myTemplate -ResourcePool $myResourcePool 
